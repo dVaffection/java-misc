221ceda3f203a114e3844e7ebeba16808c350187
dV <dv@witkit.com>
2014-03-17 18:20:20 -0700

    now sort does not return value, also added shuffle

--- src/exercises/Sorting.java
+++ src/exercises/Sorting.java
@@ -1,8 +1,11 @@
 package exercises;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Sorting {
 
-    public static <T extends Comparable<? super T>> T[] bubble(T[] array) {
+    public static <T extends Comparable<? super T>> void bubble(T[] array) {
         for (int i = 0, j = array.length; i < array.length; i++, j--) {
             for (int k = 1; k < j; k++) {
                 if (array[k - 1].compareTo(array[k]) > 0) {
@@ -12,8 +15,25 @@ public class Sorting {
                 }
             }
         }
+    }
 
-        return array;
+    public static <T extends Comparable<? super T>> T[] shuffle(T[] array) {
+        T[] copy = Arrays.copyOf(array, array.length);
+        Random random = new Random();
+
+        int n = copy.length;
+        while (n > 1) {
+            int k = random.nextInt(n);
+            n--;
+
+            T tmp = copy[n];
+            copy[n] = copy[k];
+            copy[k] = tmp;
+
+
+        }
+
+        return copy;
     }
 
 }
--- tests/exercises/SortingTest.java
+++ tests/exercises/SortingTest.java
@@ -1,23 +1,24 @@
 package exercises;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
+
+import java.util.Arrays;
 
 public class SortingTest {
 
-    private Sorting sorting;
-
-    @Before
-    public void setUp() {
-        sorting = new Sorting();
+    @Test
+    public void testBubble() {
+        Integer[] actual = {3, 2, 5, 8, 1};
+        Integer[] expected = {1, 2, 3, 5, 8};
+        Sorting.bubble(actual);
+        Assert.assertArrayEquals(expected, actual);
     }
 
     @Test
-    public void testBubble() {
-        Integer[] array = {3, 2, 5, 8, 1};
-        Integer[] expected = {1, 2, 3, 5, 8};
-        Integer[] actual = sorting.bubble(array);
-        Assert.assertArrayEquals(expected, actual);
+    public void shuffle() {
+        Integer[] array = {1, 2, 3};
+        Integer[] shuffled = Sorting.shuffle(array);
+        Assert.assertFalse(Arrays.equals(array, shuffled));
     }
 }
