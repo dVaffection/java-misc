1fa9c7dde09184621dd1086fbbada9458406c0c3
dV <dVaffection@gmail.com>
2014-03-19 23:09:56 -0700

    Replenished worker with lacking unit tests

--- tests/eerf/wrapper/WorkerTest.java
+++ tests/eerf/wrapper/WorkerTest.java
@@ -9,9 +9,9 @@ import static org.mockito.Mockito.when;
 
 public class WorkerTest {
 
-    Worker          worker;
-    ExternalProgram externalProgram;
-    Storage         storage;
+    private Worker          worker;
+    private ExternalProgram externalProgram;
+    private Storage         storage;
 
     @Before
     public void setUp() throws Exception {
@@ -22,7 +22,7 @@ public class WorkerTest {
     }
 
     @Test
-    public void fromIdleToWorking() {
+    public void fromIdleToBusy() {
         when(storage.getId()).thenReturn("123");
         when(externalProgram.wasTerminated()).thenReturn(false);
         when(externalProgram.isWorking()).thenReturn(true);
@@ -35,19 +35,102 @@ public class WorkerTest {
             Assert.fail(e.getMessage());
         }
 
-
         try {
             worker.clean();
             Assert.fail("Exception was not thrown");
         } catch (StateException e) {
-            Assert.assertTrue(true);
         }
 
-        
         try {
             worker.exec("does-not-matter", "does-not-matter");
             status = worker.scan();
             Assert.assertSame(State.StatusName.BUSY, status.getName());
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void fromBusyToTerminated() {
+        worker.changeState(new BusyState(worker, storage, externalProgram));
+
+        when(externalProgram.wasTerminated()).thenReturn(false);
+        when(externalProgram.isWorking()).thenReturn(true);
+
+        State.Status status = null;
+        try {
+            status = worker.scan();
+            Assert.assertSame(State.StatusName.BUSY, status.getName());
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        try {
+            worker.clean();
+            Assert.fail("Exception expected");
+        } catch (StateException e) {
+        }
+
+        try {
+            worker.exec("does-not-matter", "does-not-matter");
+            Assert.fail("Exception expected");
+        } catch (StateException e) {
+        }
+
+
+        when(externalProgram.wasTerminated()).thenReturn(true);
+        try {
+            status = worker.scan();
+            Assert.assertSame(State.StatusName.TERMINATED, status.getName());
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void fromBusyToOutput() {
+        worker.changeState(new BusyState(worker, storage, externalProgram));
+
+        when(externalProgram.wasTerminated()).thenReturn(false);
+        when(externalProgram.isWorking()).thenReturn(false);
+        when(externalProgram.hasOutput()).thenReturn(true);
+
+        State.Status status = null;
+        try {
+            status = worker.scan();
+            Assert.assertSame(State.StatusName.OUTPUT, status.getName());
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void fromOutputToIdle() {
+        worker.changeState(new OutputState(worker, storage, externalProgram));
+
+        State.Status status = null;
+        try {
+            status = worker.scan();
+            Assert.assertSame(State.StatusName.OUTPUT, status.getName());
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        try {
+            worker.exec("does-not-matter", "does-not-matter");
+            Assert.fail("Exception expected");
+        } catch (StateException e) {
+        }
+
+        try {
+            worker.clean();
+        } catch (StateException e) {
+            Assert.fail(e.getMessage());
+        }
+
+        try {
+            status = worker.scan();
+            Assert.assertSame(State.StatusName.IDLE, status.getName());
         } catch (StateException e) {
             Assert.fail(e.getMessage());
         }
