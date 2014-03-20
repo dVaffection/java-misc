9788b1125f0cee7f6e1e196ea9af6525087e71a3
dV <dv@witkit.com>
2014-03-19 17:40:11 -0700

    Nominal states comparison

--- src/eerf/wrapper/BaseState.java
+++ src/eerf/wrapper/BaseState.java
@@ -2,14 +2,23 @@ package eerf.wrapper;
 
 abstract class BaseState implements State {
 
-    protected Worker worker;
-    protected Storage storage;
+    protected Worker          worker;
+    protected Storage         storage;
     protected ExternalProgram externalProgram;
 
     BaseState(Worker worker, Storage storage, ExternalProgram externalProgram) {
         this.worker = worker;
         this.storage = storage;
         this.externalProgram = externalProgram;
+    }
+
+
+    /**
+     * Nominal state comparison. Check only of given state class name equals to compared one.
+     */
+    public boolean equals(Object obj) {
+        State state = (State) obj;
+        return this.getClass() == state.getClass();
     }
 
     @Override
