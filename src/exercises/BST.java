942d61c53a8a38ea653ea591c4f61fbb73e046fe
dV <dVaffection@gmail.com>
2014-04-02 22:33:00 -0700

    Added pre/in/post binary search tree traverses and inited delete node method

--- src/exercises/BST.java
+++ src/exercises/BST.java
@@ -1,21 +1,10 @@
 package exercises;
 
-import java.lang.*;
 import java.util.*;
 
 public class BST<E extends Comparable<E>> {
 
     private Node root;
-
-    class Node {
-        Node(E value) {
-            this.value = value;
-        }
-
-        public Node left;
-        public Node right;
-        public E    value;
-    }
 
     public boolean add(E e) {
         if (root == null) {
@@ -49,6 +38,30 @@ public class BST<E extends Comparable<E>> {
         return false;
     }
 
+    public boolean delete(E e) {
+//        List<E> path = new LinkedList<>();
+//
+//        traverseInOrder(root, path);
+
+        if (root == null) {
+            return false;
+        }
+
+        return delete(e, root, null);
+    }
+
+    private boolean delete(E e, Node cur, Node prev) {
+        if (cur != null) {
+            if (cur.value == e) {
+
+            } else {
+                return delete(e, cur.left, cur);
+            }
+        }
+
+        return false;
+    }
+
     public void dump() {
         LinkedHashMap<Integer, List<E>> valuesByLevels = new LinkedHashMap<>();
         collectValuesByLevels(root, 1, valuesByLevels);
@@ -74,6 +87,66 @@ public class BST<E extends Comparable<E>> {
             int rightLevel = currentLevel + 1;
             collectValuesByLevels(node.left, leftLevel, valuesByLevels);
             collectValuesByLevels(node.right, rightLevel, valuesByLevels);
+        }
+    }
+
+    public List<E> traversePreOrder() {
+        List<E> path = new LinkedList<>();
+
+        traversePreOrder(root, path);
+        return path;
+    }
+
+    private void traversePreOrder(Node node, List<E> path) {
+        if (node != null) {
+            path.add(node.value);
+
+            traversePreOrder(node.left, path);
+            traversePreOrder(node.right, path);
+        }
+    }
+
+    public List<E> traverseInOrder() {
+        List<E> path = new LinkedList<>();
+
+        traverseInOrder(root, path);
+        return path;
+    }
+
+    private void traverseInOrder(Node node, List<E> path) {
+        if (node != null) {
+            traverseInOrder(node.left, path);
+
+            path.add(node.value);
+
+            traverseInOrder(node.right, path);
+        }
+    }
+
+    public List<E> traversePostOrder() {
+        List<E> path = new LinkedList<>();
+
+        traversePostOrder(root, path);
+        return path;
+    }
+
+    private void traversePostOrder(Node node, List<E> path) {
+        if (node != null) {
+            traversePostOrder(node.left, path);
+            traversePostOrder(node.right, path);
+
+            path.add(node.value);
+        }
+    }
+
+    class Node {
+
+        public Node left;
+        public Node right;
+        public E    value;
+
+        Node(E value) {
+            this.value = value;
         }
     }
 
--- tests/exercises/BSTTest.java
+++ tests/exercises/BSTTest.java
@@ -1,8 +1,8 @@
 package exercises;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
+
+import java.util.*;
 
 public class BSTTest {
 
@@ -22,22 +22,63 @@ public class BSTTest {
         Assert.assertFalse(bst.add(6));
     }
 
+    private List<Integer> getValues() {
+        return Arrays.asList(8, 3, 3, 6, 6, 10, 14, 1, 7, 13, 4);
+    }
+
     @Test
     public void dump() {
-        bst.add(8);
-        bst.add(3);
-        bst.add(3);
-        bst.add(6);
-        bst.add(6);
-        bst.add(14);
-        bst.add(13);
-        bst.add(1);
-        bst.add(7);
-        bst.add(10);
-        bst.add(4);
-        bst.add(16);
-        bst.add(2);
+        List<Integer> values = getValues();
+
+        for (Integer value : values) {
+            bst.add(value);
+        }
 
         bst.dump();
+    }
+
+    @Test
+    public void traversePreOrder() {
+        List<Integer> values = getValues();
+
+        for (Integer value : values) {
+            bst.add(value);
+        }
+
+        List<Integer> expected = Arrays.asList(8, 3, 1, 6, 4, 7, 10, 14, 13);
+        List<Integer> path = bst.traversePreOrder();
+        Assert.assertEquals(expected, path);
+
+        System.out.println("Pre-order: " + path);
+    }
+
+    @Test
+    public void traverseInOrder() {
+        List<Integer> values = getValues();
+
+        for (Integer value : values) {
+            bst.add(value);
+        }
+
+        List<Integer> expected = Arrays.asList(1, 3, 4, 6, 7, 8, 10, 13, 14);
+        List<Integer> path = bst.traverseInOrder();
+        Assert.assertEquals(expected, path);
+
+        System.out.println("In-order: " + path);
+    }
+
+    @Test
+    public void traversePostOrder() {
+        List<Integer> values = getValues();
+
+        for (Integer value : values) {
+            bst.add(value);
+        }
+
+        List<Integer> expected = Arrays.asList(1, 4, 7, 6, 3, 13, 14, 10, 8);
+        List<Integer> path = bst.traversePostOrder();
+        Assert.assertEquals(expected, path);
+
+        System.out.println("Post-order: " + path);
     }
 }
