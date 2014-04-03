d44c75f878616c15bfcc3d1f43fe6a9da682119d
dV <dv@witkit.com>
2014-04-02 18:17:05 -0700

    small changes to Sets excercises

--- src/exercises/Sets.java
+++ src/exercises/Sets.java
@@ -4,9 +4,8 @@ import java.util.*;
 
 public class Sets {
 
-    public <E> Collection<E> orderedNoDuplicates(Collection<E> c) {
-        Collection<E> result = new TreeSet<>(c);
-        return result;
+    public <E> Set<E> orderedNoDuplicates(Collection<E> c) {
+        return new TreeSet<>(c);
     }
 
 }
--- tests/exercises/SetsTest.java
+++ tests/exercises/SetsTest.java
@@ -18,8 +18,8 @@ public class SetsTest {
     @Test
     public void orderedNoStringDuplicates() {
         Collection<String> c = Arrays.asList("d", "d", "c", "a", "b", "a", "c");
-        Collection<String> actual = sets.orderedNoDuplicates(c);
-        Collection<String> expected = new TreeSet<>(Arrays.asList("a", "b", "c", "d"));
+        Set<String> actual = sets.orderedNoDuplicates(c);
+        Set<String> expected = new TreeSet<>(Arrays.asList("a", "b", "c", "d"));
 
         assertEquals(expected, actual);
     }
@@ -27,8 +27,8 @@ public class SetsTest {
     @Test
     public void orderedNoIntegerDuplicates() {
         Collection<Integer> c = Arrays.asList(4, 4, 3, 1, 2, 4, 2, 1);
-        Collection<Integer> actual = sets.orderedNoDuplicates(c);
-        Collection<Integer> expected = new TreeSet<>(Arrays.asList(1, 2, 3, 4));
+        Set<Integer> actual = sets.orderedNoDuplicates(c);
+        Set<Integer> expected = new TreeSet<>(Arrays.asList(1, 2, 3, 4));
 
         assertEquals(expected, actual);
     }
