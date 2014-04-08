f97d7e0a3e0ca0940d4d0dee86332830be58ef47
dV <dVaffection@gmail.com>
2014-04-08 00:26:11 -0700

    now we know how to return a sublist from a list

--- tests/exercises/Array3Test.java
+++ tests/exercises/Array3Test.java
@@ -1,8 +1,10 @@
 package exercises;
 
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.*;
 
 public class Array3Test {
 
@@ -17,5 +19,15 @@ public class Array3Test {
     public void testMaxMirror() {
         int[] nums = {1, 2, 1, 4};
         Assert.assertEquals(3, array3.maxMirror(nums));
+    }
+
+    @Test
+    public void clearSubRange() {
+        List<Character> actual = new ArrayList<>(Arrays.asList('a', 'b', 'c', 'd', 'e'));
+        actual.subList(1, 3).clear();
+
+        List<Character> expected = Arrays.asList('a', 'd', 'e');
+
+        Assert.assertEquals(expected, actual);
     }
 }
